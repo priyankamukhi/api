2b2796258c7a782859e856db19eed55c9f0ae1ec
Priyanka_Mukhi <priyankamukhi96@gmail.com>
2022-01-05 10:22:21 +0530

    added

---
+++ webapi/Controllers/webapiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    public class webapiController : ApiController
+    {
+        IList<webapi> webapi = new List<webapi>[]
+        {
+            new webapi{ }
+
+        }
+
+    
+}
+
---
+++ webapi/Models/webapi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapi.Models
+{
+    public class webapi
+    {
+       public int stdid { get; set; }
+        public  string name { get; set; }
+        public string Class { get;set;}
+        public int rollno { get; set; }
+        public string subject { get; set; }
+    }
+}
